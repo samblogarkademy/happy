a307062c83dab3982cf4967cf9fd5ddca1e04e49
samsudin <60283548+samblogarkademy@users.noreply.github.com>
2023-12-31 20:33:31 +0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import Particles from "react-particles";
+import {loadFireworksPreset} from "tsparticles-preset-fireworks"
+import { Typewriter } from "react-simple-typewriter";
+import { useState } from "react";
+import Countdown from "react-countdown";
 
 function App() {
+  const [newYearMassage, setNewYearMassage] = useState (["Hitung Mundur bareng yuk!!!😁😁😁"])
+
+  const particleInitilization = async(engine) => {
+    await loadFireworksPreset(engine)
+  }
+
+  function timeLeft(){
+    const newYearDate = new Date("January 1, 2024 00:00:00").getTime()
+    const nowDate = new Date().getTime()
+    const remainingTime = newYearDate - nowDate
+    return remainingTime
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <Particles
+    init ={particleInitilization}
+    options={{preset: "fireworks"}} />
+    <div className="flex flex-col justify-center items-center min-h-screen gap-5">
+      <span className="text-white text text-4xl font-bold z-50 px-4">
+        <Typewriter 
+        words={newYearMassage}
+        loop={false}
+        cursorStyle="_"
+        cursor
+        typeSpeed={10}
+        deleteSpeed={10}
+
+    
+        
+        />
+        </span>
+        <div className="z-50 text-white font-bold text-2xl:">
+          <Countdown date={Date.now() + timeLeft()} onComplete={() => setNewYearMassage([
+            "Selamat", "Tahun", "Baru", "2024", "Sifa 🥳"
+          ])}
+           typeSpeed={10}
+           deleteSpeed={5}
+          
+          />
+        </div>
     </div>
-  );
+    </>
+    );
 }
 
 export default App;
